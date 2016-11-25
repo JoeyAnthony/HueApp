7459712584be8b276a44b2ae091eb082bd0d6845
DESKTOP-S8PIJ7G\Joey <jvanhoek@hotmail.com>
2016-11-25 11:48:17 +0100

    commit

---
+++ Hue/HueApp/Lamp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hue
+{
+    class Lamp
+    {
+        private int Number;
+        public string Name;
+
+        public Lamp()
+        {
+        }
+
+        public Lamp(int num, string name)
+        {
+            Number = num;
+            Name = name;
+        }
+    }
+}
